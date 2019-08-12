3e3c097ae80fc4824291c9d19deb976bd2e7e7db
高文杰 <yidong-ios16>
2019-08-12 14:38:39 +0800

    changed

---
+++ 02-分量加载/demo2/SubPack/SubPack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SubPack
+//
+//  Created by wenjie on 2019/8/12.
+//  Copyright © 2019 wenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 02-分量加载/demo2/SubPack/SubPack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SubPack
+//
+//  Created by wenjie on 2019/8/12.
+//  Copyright © 2019 wenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
